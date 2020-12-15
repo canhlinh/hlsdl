390c681bd5db0656ce9a27eacc5393aa57ecdbd7
canhlinh <canhlinh.lienson@gmail.com>
2020-12-15 15:00:18 +0700

    Fix test

--- hlsdl_test.go
+++ hlsdl_test.go
@@ -12,22 +12,23 @@ func TestDescrypt(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	hlsDl := New("https://cdn.theoplayer.com/video/big_buck_bunny_encrypted/stream-800/index.m3u8", "./download", 2, false)
+	hlsDl := New("https://cdn.theoplayer.com/video/big_buck_bunny_encrypted/stream-800/index.m3u8", "./", 2, false)
 
 	seg := segs[0]
 	seg.Path = fmt.Sprintf("%s/seg%d.ts", hlsDl.dir, seg.SeqId)
 	if err := hlsDl.downloadSegment(seg); err != nil {
 		t.Fatal(err)
 	}
+	defer os.Remove(seg.Path)
 
-	if _, err := hlsDl.Decrypt(seg); err != nil {
+	if _, err := hlsDl.decrypt(seg); err != nil {
 		t.Fatal(err)
 	}
 }
 
 func TestDownload(t *testing.T) {
 
-	hlsDl := New("https://cdn.theoplayer.com/video/big_buck_bunny_encrypted/stream-800/index.m3u8", "./download", 2, false)
+	hlsDl := New("https://cdn.theoplayer.com/video/big_buck_bunny_encrypted/stream-800/index.m3u8", "./", 2, false)
 	filepath, err := hlsDl.Download()
 	if err != nil {
 		t.Fatal(err)
